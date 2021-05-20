e3c16a4ab7673e92a6cc4013df8975f3e98030a3
Amaury137 <amaury137@DESKTOP-PQHT4TE.localdomain>
2021-05-19 21:29:55 -0500

    Activity_04 finished

---
+++ unit1/Activity_04.py
@@ -0,0 +1,13 @@
+num_1 = 0
+num_2 = 0
+addition = 0
+multiplication = 0
+
+num_1 = int(input("enter number 1: "))
+num_2 = int(input("enter number 2: "))
+
+add = num_1+num_2
+multiply = num_1*num_2
+
+print("sum: ", add)
+print("multiplication: ", multiply)
